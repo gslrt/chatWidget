3db53970859a30c912b53ae44279876af7b20705
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-18 15:34:49 +0200

    Update chat.js

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,92 +1,91 @@
-// routes/chat.js
 
 const express = require('express');
-const router = express.Router();
+const http = require('http');
+const socketIo = require('socket.io');
 const fetch = require('node-fetch');
 const { generateAudio } = require('./tts.js');
-const db = require('./db');  // Assuming you'll set up a basic db.js for PostgreSQL connection using pg-promise
+const { Pool } = require('pg');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 
-router.post("/", async (req, res) => {
-    const userInput = req.body.question;
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
-    console.log("User Input:", userInput);
+app.use(session({
+    store: new RedisStore({ url: process.env.REDIS_URL }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
 
-    function getCurrentDateTime(timezone = 'Europe/Berlin') {
-        const date = new Date();
-        const options = {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit',
-            timeZone: timezone,
-            hour12: false
-        };
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
 
-        const formatter = new Intl.DateTimeFormat('en-US', options);
-        return formatter.format(date);
-    }
+    socket.on('chatMessage', async (data) => {
+        const userInput = data.question;
+        const currentTimestamp = new Date();
+        const hiveAccess = process.env.HIVE_ACCESS_PUBLIC;
+        const url = process.env.CHAT_URL + hiveAccess;
+        let systemMessage = `you are a pirate`;
 
-    const hiveAccess = process.env.HIVE_ACCESS_PUBLIC;  // Keeping only public access as default
-    const url = process.env.CHAT_URL + hiveAccess;
-
-    let systemMessage = `you are a pirate`;
-
-    const dataToSend = {
-        question: userInput,
-        overrideConfig: {
-            systemMessage,
-            openAIApiKey: process.env.OPENAI_API_KEY,
-            pineconeEnv: process.env.PINECONE_ENVIRONMENT,
-            pineconeApiKey: process.env.PINECONE_API_KEY,
-            pineconeNamespace: process.env.PINECONE_NAMESPACE,
-            pineconeIndex: process.env.PINECONE_INDEX_NAME
+       const dataToSend = {
+            question: userInput,
+            overrideConfig: {
+                systemMessage,
+                openAIApiKey: process.env.OPENAI_API_KEY,
+                pineconeEnv: process.env.PINECONE_ENVIRONMENT,
+                pineconeApiKey: process.env.PINECONE_API_KEY,
+                pineconeNamespace: process.env.PINECONE_NAMESPACE,
+                pineconeIndex: process.env.PINECONE_INDEX_NAME
         }
     };
 
-    // Log the data being sent to the chatbot
-    console.log("Sending data to chatbot:", dataToSend);
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${process.env.FLOWISE_API_KEY}`,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(dataToSend)
+            });
+            const responseBody = await response.json();
+            let aiResponse = responseBody;
 
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${process.env.FLOWISE_API_KEY}`,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(dataToSend)
-        });
+            // Database Logging & Session Handling
+            if (!socket.request.session.conversation_id) {
+                const result = await pool.query('INSERT INTO chat_Conversations (start_timestamp, ip_address, user_agent) VALUES ($1, $2, $3) RETURNING conversation_id', [currentTimestamp, socket.request.ip, socket.request.headers['user-agent']]);
+                socket.request.session.conversation_id = result.rows[0].conversation_id;
+            }
 
-        const responseBody = await response.json();
+            await pool.query('INSERT INTO chat_Messages (conversation_id, timestamp, direction, content) VALUES ($1, $2, $3, $4)', [socket.request.session.conversation_id, currentTimestamp, 'sent', userInput]);
+            await pool.query('INSERT INTO chat_Messages (conversation_id, timestamp, direction, content) VALUES ($1, $2, $3, $4)', [socket.request.session.conversation_id, currentTimestamp, 'received', aiResponse]);
+            await pool.query('UPDATE chat_Conversations SET end_timestamp = $1 WHERE conversation_id = $2', [currentTimestamp, socket.request.session.conversation_id]);
 
-        let aiResponse = responseBody;
-        console.log('AI response:', aiResponse);
-        let responseObject = { 'text': aiResponse };
+            let responseObject = { 'text': aiResponse };
+            if (data.ttsEnabled) {
+                let audioUrl = await generateAudio(aiResponse);
+                responseObject['audioUrl'] = audioUrl;
+            }
 
-        // Begin database logging
-        // Start a conversation
-        const startTimestamp = new Date();
-        const conversationResult = await db.none('INSERT INTO Conversations (start_timestamp, ip_address) VALUES ($1, $2)', [startTimestamp, req.ip]);
+            // Emit the response to the user via socket
+            socket.emit('botResponse', responseObject);
 
-        // Log the message
-        await db.none('INSERT INTO Messages (conversation_id, timestamp, direction, content) VALUES ($1, $2, $3, $4)', [conversationResult.conversation_id, startTimestamp, 'sent', userInput]);
-        await db.none('INSERT INTO Messages (conversation_id, timestamp, direction, content) VALUES ($1, $2, $3, $4)', [conversationResult.conversation_id, new Date(), 'received', aiResponse]);
-
-        // Here you'd ideally generate an engagement report based on the conversation and store it. This is a placeholder.
-        // await generateEngagementReport(conversationResult.conversation_id);
-
-        // End database logging
-
-        if (req.body.ttsEnabled) {
-            let audioUrl = await generateAudio(aiResponse);
-            responseObject['audioUrl'] = audioUrl;
+        } catch (fetchError) {
+            console.error('Fetch error:', fetchError);
+            socket.emit('error', { error: 'Failed to fetch from the API' });
         }
-
-        res.send(responseObject);
-    } catch (fetchError) {
-        console.error('Fetch error:', fetchError);
-        res.status(500).send({ error: 'Failed to fetch from the API' });
-    }
+    });
 });
 
-module.exports = router;
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
+
