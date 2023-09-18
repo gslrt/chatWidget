03d6f4de315535f743b271ebede072854a50c39b
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-18 19:21:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 // server.js
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const chatRoute = require('./routes/chat');
+
+const app = express();
+
+// Middlewares
+app.use(bodyParser.json());
+
+// Routes
+app.use('/chat', chatRoute);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
