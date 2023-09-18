96b6173ec7319cb9e4ccbee6887f9b6272b4b5e1
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-18 20:14:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const http = require('http');
+const socketIo = require('socket.io');
 const { generateAudio } = require('./backend/models/tts.js');  
 const chatRoute = require('./backend/routes/chat.js');
-const io = socketIo(server);
-const app = express();
-
-
 const redis = require('redis');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 const redisClient = redis.createClient({
     url: process.env.REDIS_URL
@@ -25,15 +27,21 @@ app.use(session({
     saveUninitialized: false
 }));
 
-
 // Middlewares
 app.use(bodyParser.json());
 
 // Routes
 app.use('/chat', chatRoute);
 
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+    
+    // Pass the socket to your chatRoute logic or any other routes that need it
+    chatRoute.handleSocketConnection(socket);
+});
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
