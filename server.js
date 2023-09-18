86ad7ffd8416bd2ee98639e3201b8f6169a4f8c0
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-18 19:52:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ const chatRoute = require('./backend/routes/chat.js');
 
 const app = express();
 
+
+const redis = require('redis');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+
+const redisClient = redis.createClient({
+    url: process.env.REDIS_URL
+});
+
+app.use(session({
+    store: new RedisStore({ client: redisClient }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+
 // Middlewares
 app.use(bodyParser.json());
 
