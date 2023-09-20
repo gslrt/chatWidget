fcd2f0978b495e19f7d2205f43b532866827e4fd
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-20 19:21:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,61 +8,66 @@ const redis = require('redis');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const cors = require('cors');
-const fs = require('fs');  // File System for logging
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL
+    url: process.env.REDIS_URL
 });
 
+// Set up session storage
 app.use(session({
-  store: new RedisStore({ client: redisClient }),
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
+    store: new RedisStore({ client: redisClient }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
 }));
 
+// Set up CORS
 const whitelist = process.env.CORS_WHITELIST_WIDGET.split(',');
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
     }
-  }
 };
 app.use(cors(corsOptions));
 
+// Middlewares
 app.use(bodyParser.json());
 
-const volumePath = process.env.RAILWAY_VOLUME_MOUNT_PATH || 'frontend/dist';
-app.use('/frontend/dist', express.static(path.join(__dirname, volumePath)));
+// Serve static files for the frontend from the /oxofrmbl directory
+app.use('/frontend/dist', express.static('/oxofrmbl'));
 
+// Serve static files for the frontend from the frontend/dist directory
+app.use('/frontend/dist', express.static(path.join(__dirname, 'frontend/dist')));
+
+// Routes
 app.use('/chat', chatRoute);
 
 io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
-  chatRoute.handleSocketConnection(socket);
+    console.log('User connected:', socket.id);
+    
+    // Pass the socket to your chatRoute logic or any other routes that need it
+    chatRoute.handleSocketConnection(socket);
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  
-  // Log the contents of the dist directory
-  fs.readdir(volumePath, (err, files) => {
-    if (err) {
-      console.error("Could not list the directory.", err);
-    } else {
-      console.log("Contents of dist directory:", files);
-    }
-  });
-
-  // Log the webpack output path
-  console.log(`Webpack output path: ${path.resolve(__dirname, volumePath)}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
+// Log the contents of /oxofrmbl
+fs.readdir('/oxofrmbl', (err, files) => {
+    console.log('Contents of /oxofrmbl:', files);
+});
+
+// Log the webpack output path
+console.log('Webpack output path:', path.resolve(__dirname, '/oxofrmbl'));
