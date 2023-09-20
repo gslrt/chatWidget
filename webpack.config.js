584f720b45d575472609e0404abdfcf27109c4e2
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-20 20:00:28 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
 // webpack.config.js
 const path = require('path');
 
-const volumePath = process.env.RAILWAY_VOLUME_MOUNT_PATH || '/oxofrmbl';
-
 module.exports = {
   entry: {
     native: './frontend/src/native/nativeBundle.js',
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, volumePath),
+    path: path.resolve(__dirname, process.env.RAILWAY_VOLUME_MOUNT_PATH || '/oxofrmbl'),
   },
 };
