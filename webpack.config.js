5d5d569331deb0e61e5ed1635aa8962e9470ff64
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-20 19:05:34 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,11 @@ const path = require('path');
 module.exports = {
   entry: {
     native: './frontend/src/native/nativeBundle.js',
+    // Add other bundles as needed
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, '/oxofrmbl') // Change this line to match the desired directory
+    path: path.resolve(__dirname, '/oxofrmbl')  // Update this to your volume path
   }
 };
+
