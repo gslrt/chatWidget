5496c9c410397bfa31062d63b8d79255081d4713
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-21 13:00:58 +0200

    Update chat.js

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,5 +1,4 @@
 // backend/routes/chat.js
-
 const fetch = require('node-fetch');
 const { generateAudio } = require('../models/tts.js');
 const { Pool } = require('pg');
@@ -12,22 +11,17 @@ const pool = new Pool({
     },
 });
 
-// Construct the complete Chat URL from environment variables
-const CHAT_URL_BASE = process.env.CHAT_URL;
-const HIVE_ACCESS_PUBLIC = process.env.HIVE_ACCESS_PUBLIC;
-const completeChatUrl = `${CHAT_URL_BASE}${HIVE_ACCESS_PUBLIC}`;
-const FLOWISE_API_KEY = process.env.FLOWISE_API_KEY;
-
 router.handleSocketConnection = (socket, uid) => {
     console.log(`[Chat Route] User ${uid} connected: ${socket.id}`);
 
     socket.on('chatMessage', async (data) => {
         try {
             console.log("[Chat Route] Received chat message:", data);
-
             const userInput = data.question;
             const currentTimestamp = new Date();
-            const systemMessage = "you are a pirate";
+            const hiveAccess = process.env.HIVE_ACCESS_PUBLIC;
+            const url = process.env.CHAT_URL + hiveAccess;
+            let systemMessage = `you are a pirate`;
 
             const dataToSend = {
                 question: userInput,
@@ -38,29 +32,28 @@ router.handleSocketConnection = (socket, uid) => {
                     pineconeApiKey: process.env.PINECONE_API_KEY,
                     pineconeNamespace: process.env.PINECONE_NAMESPACE,
                     pineconeIndex: process.env.PINECONE_INDEX_NAME
-                },
+                }
             };
 
-            const response = await fetch(completeChatUrl, {
+            const response = await fetch(url, {
                 method: "POST",
                 headers: {
-                    "Authorization": `Bearer ${FLOWISE_API_KEY}`,
-                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${process.env.FLOWISE_API_KEY}`,
+                    "Content-Type": "application/json"
                 },
-                body: JSON.stringify(dataToSend),
+                body: JSON.stringify(dataToSend)
             });
 
             const responseBody = await response.json();
             let aiResponse = responseBody;
 
             if (data.ttsEnabled) {
-                const audioUrl = await generateAudio(aiResponse);
-                socket.emit('botResponse', { text: aiResponse, audioUrl });
+                let audioUrl = await generateAudio(aiResponse);
+                socket.emit('botResponse', { 'text': aiResponse, 'audioUrl': audioUrl });
             } else {
-                socket.emit('botResponse', { text: aiResponse });
+                socket.emit('botResponse', { 'text': aiResponse });
             }
 
-            // Database operations
             if (!socket.request.session.conversation_id) {
                 const result = await pool.query('INSERT INTO chat_Conversations (start_timestamp, ip_address, user_agent) VALUES ($1, $2, $3) RETURNING conversation_id', [currentTimestamp, socket.request.ip, socket.request.headers['user-agent']]);
                 socket.request.session.conversation_id = result.rows[0].conversation_id;
@@ -69,10 +62,10 @@ router.handleSocketConnection = (socket, uid) => {
             await pool.query('INSERT INTO chat_Messages (conversation_id, timestamp, direction, content) VALUES ($1, $2, $3, $4)', [socket.request.session.conversation_id, currentTimestamp, 'sent', userInput]);
             await pool.query('INSERT INTO chat_Messages (conversation_id, timestamp, direction, content) VALUES ($1, $2, $3, $4)', [socket.request.session.conversation_id, currentTimestamp, 'received', aiResponse]);
             await pool.query('UPDATE chat_Conversations SET end_timestamp = $1 WHERE conversation_id = $2', [currentTimestamp, socket.request.session.conversation_id]);
-            
+
         } catch (error) {
             console.error('Error:', error);
-            socket.emit('error', { error: 'An error occurred' });
+            socket.emit('error', { error: 'Failed to handle the chat message' });
         }
     });
 };
