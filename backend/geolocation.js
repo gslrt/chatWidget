6fbf2eaea349e98bb11834f2c4be4467907e421b
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-24 13:47:52 +0200

    Create geolocation.js

---
+++ backend/geolocation.js
@@ -0,0 +1,26 @@
+// backend/geolocation.js
+
+const fetch = require('node-fetch');
+
+async function getGeolocation(ipAddress) {
+  const apiKey = process.env.GEOLOCATOR_API_KEY;
+  const url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ipAddress}`;
+
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      console.error('Failed to fetch geolocation:', response.status, response.statusText);
+      return null;
+    }
+  } catch (error) {
+    console.error('An error occurred while fetching geolocation:', error);
+    return null;
+  }
+}
+
+module.exports = {
+  getGeolocation
+};
