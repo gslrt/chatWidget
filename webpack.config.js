aa5d000132f9631d9b5f8611cda51b7f90b53a9c
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-24 19:19:13 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,10 @@ const path = require('path');
 module.exports = {
   entry: {
     native: './frontend/src/native/nativeBundle.js',
+    tether: './frontend/src/tether.js',
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, process.env.RAILWAY_VOLUME_MOUNT_PATH || '/oxofrmbl'),
+    path: path.resolve(__dirname, process.env.RAILWAY_VOLUME_MOUNT_PATH),
   },
 };
