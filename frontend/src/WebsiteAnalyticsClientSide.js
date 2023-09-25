4ae5d0e78767d1f3900978a1b8ad19fc04ff58c9
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-25 18:52:26 +0200

    Update WebsiteAnalyticsClientSide.js

--- frontend/src/WebsiteAnalyticsClientSide.js
+++ frontend/src/WebsiteAnalyticsClientSide.js
@@ -1,29 +1,11 @@
-// /frontend/src/WebsiteAnalyticsClientSide.js
 
 
-// Function to track time spent on a page
-const trackTimeOnPage = () => {
-  const startTime = new Date().getTime();
-  window.addEventListener('beforeunload', () => {
-    const endTime = new Date().getTime();
-    const timeSpent = endTime - startTime;
-    // Send this data to the server via API call to /analytics/trackTimeOnPage
-  });
-};
+// frontend/src/tether.js
 
-// Function to track scroll behavior
-const trackScrollBehavior = () => {
-  window.addEventListener('scroll', () => {
-    const scrollPosition = window.scrollY;
-    // Send this data to the server via API call to another endpoint
-  });
-};
-
-// Add other tracking functions here
-
-export {
-  trackTimeOnPage,
-  trackScrollBehavior,
-  // Export other tracking functions here
-};
-
+// Function to load additional scripts asynchronously
+function loadScript(src) {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = src;
+  document.body.appendChild(script);
+}
