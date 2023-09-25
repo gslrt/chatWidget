0f48dd0f5cbbd84e1f22b69a078efc5b50d6e835
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-25 19:16:22 +0200

    Update WebsiteAnalyticsClientSide.js

--- frontend/src/WebsiteAnalyticsClientSide.js
+++ frontend/src/WebsiteAnalyticsClientSide.js
@@ -3,26 +3,26 @@
 
 // Function to track time spent on a page
 const trackTimeOnPage = () => {
+  console.log("Track Time on Page function executed");
   const startTime = new Date().getTime();
   window.addEventListener('beforeunload', () => {
     const endTime = new Date().getTime();
     const timeSpent = endTime - startTime;
+    console.log(`Time Spent on Page: ${timeSpent}`);
     // Send this data to the server via API call to /analytics/trackTimeOnPage
   });
 };
 
 // Function to track scroll behavior
 const trackScrollBehavior = () => {
+  console.log("Track Scroll Behavior function executed");
   window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
+    console.log(`Scroll Position: ${scrollPosition}`);
     // Send this data to the server via API call to another endpoint
   });
 };
 
-// Add other tracking functions here
-
-export {
-  trackTimeOnPage,
-  trackScrollBehavior,
-  // Export other tracking functions here
-};
+// Call the tracking functions
+trackTimeOnPage();
+trackScrollBehavior();
