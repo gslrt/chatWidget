c9335d9a9a4780ea361b8a5eb6c17b2279a6fca8
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-25 19:02:46 +0200

    Update WebsiteAnalyticsClientSide.js

--- frontend/src/WebsiteAnalyticsClientSide.js
+++ frontend/src/WebsiteAnalyticsClientSide.js
@@ -18,3 +18,11 @@ const trackScrollBehavior = () => {
     // Send this data to the server via API call to another endpoint
   });
 };
+
+// Add other tracking functions here
+
+export {
+  trackTimeOnPage,
+  trackScrollBehavior,
+  // Export other tracking functions here
+};
