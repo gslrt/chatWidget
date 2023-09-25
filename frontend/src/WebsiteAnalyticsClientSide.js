2cf2a4226dcec822b2b1232b7b87122a8ac09ba4
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-25 19:11:53 +0200

    Update WebsiteAnalyticsClientSide.js

--- frontend/src/WebsiteAnalyticsClientSide.js
+++ frontend/src/WebsiteAnalyticsClientSide.js
@@ -1,3 +1,28 @@
-// HelloWorld.js
+// /frontend/src/WebsiteAnalyticsClientSide.js
 
-console.log("Hello, World!");
+
+// Function to track time spent on a page
+const trackTimeOnPage = () => {
+  const startTime = new Date().getTime();
+  window.addEventListener('beforeunload', () => {
+    const endTime = new Date().getTime();
+    const timeSpent = endTime - startTime;
+    // Send this data to the server via API call to /analytics/trackTimeOnPage
+  });
+};
+
+// Function to track scroll behavior
+const trackScrollBehavior = () => {
+  window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    // Send this data to the server via API call to another endpoint
+  });
+};
+
+// Add other tracking functions here
+
+export {
+  trackTimeOnPage,
+  trackScrollBehavior,
+  // Export other tracking functions here
+};
