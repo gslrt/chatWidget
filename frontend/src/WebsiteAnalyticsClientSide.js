6b09fcc890098befd8904c10d1abdeed29fb3357
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-25 23:18:46 +0200

    Update WebsiteAnalyticsClientSide.js

--- frontend/src/WebsiteAnalyticsClientSide.js
+++ frontend/src/WebsiteAnalyticsClientSide.js
@@ -1,28 +1,85 @@
 // /frontend/src/WebsiteAnalyticsClientSide.js
 
 
-// Function to track time spent on a page
-const trackTimeOnPage = () => {
-  console.log("Track Time on Page function executed");
-  const startTime = new Date().getTime();
-  window.addEventListener('beforeunload', () => {
-    const endTime = new Date().getTime();
-    const timeSpent = endTime - startTime;
-    console.log(`Time Spent on Page: ${timeSpent}`);
-    // Send this data to the server via API call to /analytics/trackTimeOnPage
+// Function to send analytics data to the backend
+function sendAnalyticsData(eventType, additionalInfo) {
+  const SERVICE_URL = process.env.SERVICE_URL || "http://localhost:3000";
+  fetch(`${SERVICE_URL}/analytics`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      eventType: eventType,
+      additionalInfo: additionalInfo
+    })
+  }).then(response => {
+    // Handle response here
   });
-};
+}
 
-// Function to track scroll behavior
-const trackScrollBehavior = () => {
-  console.log("Track Scroll Behavior function executed");
-  window.addEventListener('scroll', () => {
-    const scrollPosition = window.scrollY;
-    console.log(`Scroll Position: ${scrollPosition}`);
-    // Send this data to the server via API call to another endpoint
+// Track initial page view
+sendAnalyticsData("page_view", { url: window.location.href });
+
+// Heartbeat logic
+let elapsedTime = 0;
+
+function heartbeat() {
+  sendAnalyticsData("heartbeat", { timestamp: new Date().toISOString() });
+
+  let interval;
+
+  if (elapsedTime < 10) {
+    interval = 1000;
+  } else if (elapsedTime < 60) {
+    interval = 2000;
+  } else if (elapsedTime < 120) {
+    interval = 10000;
+  } else if (elapsedTime < 240) {
+    interval = 20000;
+  } else {
+    interval = 30000;
+  }
+
+  setTimeout(heartbeat, interval);
+  elapsedTime += interval / 1000;
+}
+
+// Start the heartbeat
+heartbeat();
+
+// Supported event types
+const eventTypes = ["click", "mouseover", "focus", "mouseleave", "keydown"];
+
+// Attach event listeners
+eventTypes.forEach((eventType) => {
+  document.addEventListener(eventType, function(event) {
+    const expectedEventType = event.target.getAttribute("data-analytics-event-type");
+    if (expectedEventType && eventType !== expectedEventType) {
+      return;
+    }
+
+    const trigger = event.target.getAttribute("data-analytics-event-trigger");
+    if (trigger) {
+      sendAnalyticsData(eventType, { trigger: trigger, event_type: eventType });
+    }
   });
-};
+});
 
-// Call the tracking functions
-trackTimeOnPage();
-trackScrollBehavior();
+// Intersection Observer for 'element scrolled into view'
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const trigger = entry.target.getAttribute("data-analytics-event-trigger");
+      if (trigger) {
+        sendAnalyticsData("element_scrolled_into_view", { trigger: trigger });
+      }
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+// Observe elements with 'data-analytics-event-trigger' attribute
+document.querySelectorAll('[data-analytics-event-trigger]').forEach(element => {
+  observer.observe(element);
+});
