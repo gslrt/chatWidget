ccb7145bf07682baf35e2952c9b5f29933114228
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-25 23:26:59 +0200

    Update analyticsRoutes.js

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const { createWebsiteSession, updateWebsiteSession, logWebsiteEvent } = require('../WebsiteAnalyticsServerSide');
 
-router.post('/trackTimeOnPage', async (req, res) => {
-    const { timeSpent } = req.body;
-    const ipAddress = req.ip;
-    const userAgent = req.get('User-Agent');
-    const site = 'example.com';  // Get this from request if possible
-
-    const sessionId = await createWebsiteSession(ipAddress, userAgent, site);
-    await logWebsiteEvent(sessionId, 'time_spent_on_page', { timeSpent });
-
-    res.status(200).json({ message: 'Time spent on page logged' });
+// Route to handle page views
+router.post('/page_view', (req, res) => {
+  const { url, referrer } = req.body;
+  // Store the data in your database
+  res.status(200).send('Page view recorded');
 });
 
-// Add more endpoints as needed
+// Route to handle heartbeat
+router.post('/heartbeat', (req, res) => {
+  const { timestamp } = req.body;
+  // Store the data in your database
+  res.status(200).send('Heartbeat recorded');
+});
+
+// Route to handle user interactions
+router.post('/interaction', (req, res) => {
+  const { trigger, event_type } = req.body;
+  // Store the data in your database
+  res.status(200).send('Interaction recorded');
+});
+
+// Route to handle elements scrolled into view
+router.post('/element_scrolled', (req, res) => {
+  const { trigger } = req.body;
+  // Store the data in your database
+  res.status(200).send('Element scrolled into view recorded');
+});
 
 module.exports = router;
