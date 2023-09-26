9609bd0f8fea552ad6576615ea0ce7d2d7c8383d
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-26 18:24:09 +0200

    Update WebsiteAnalyticsServerSide.js

--- backend/WebsiteAnalyticsServerSide.js
+++ backend/WebsiteAnalyticsServerSide.js
@@ -1,8 +1,9 @@
 // /backend/WebsiteAnalyticsServerSide.js
 
 
+const { v4: uuidv4 } = require('uuid');
 const { Pool } = require('pg');
-const { getGeolocation } = require('../geolocation');  
+const { getGeolocation } = require('../geolocation');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -11,16 +12,16 @@ const pool = new Pool({
   }
 });
 
-// Function to update analytics database and session
-const updateAnalyticsDatabaseAndSession = async (req, eventType, eventData) => {
-  const clientIp = req.ip; 
+// Function to initiate a new session and update the analytics database
+const initiateNewSession = async (req) => {
+  // Generate a unique session ID using UUID
+  const sessionId = uuidv4();
+
+  const clientIp = req.ip;
   const userAgent = req.headers['user-agent'];
 
   // Get geolocation data
   let geoInfo = await getGeolocation(clientIp);
-
-  // Extract session ID
-  const sessionId = req.sessionID;
 
   // Determine device type
   let deviceType = "desktop";
@@ -34,9 +35,7 @@ const updateAnalyticsDatabaseAndSession = async (req, eventType, eventData) => {
   const sessionQuery = 'INSERT INTO website_analytics_sessions(session_id, user_ip, user_agent, start_timestamp, city, country, state_prov, local_time, device_type) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)';
   await pool.query(sessionQuery, [sessionId, clientIp, userAgent, new Date(), geoInfo.city, geoInfo.country, geoInfo.state_prov, geoInfo.local_time, deviceType]);
 
-  // SQL query to insert into the events table
-  const eventQuery = 'INSERT INTO website_analytics_events(session_id, event_type, additional_info) VALUES($1, $2, $3)';
-  await pool.query(eventQuery, [sessionId, eventType, JSON.stringify(eventData)]);
+  return sessionId;
 };
 
-module.exports = { updateAnalyticsDatabaseAndSession };
+module.exports = { initiateNewSession };
