b7b6c18d85ea4c37cb29ba44bdf87956f03f2fc3
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-26 21:40:19 +0200

    Update WebsiteAnalyticsServerSide.js

--- backend/WebsiteAnalyticsServerSide.js
+++ backend/WebsiteAnalyticsServerSide.js
@@ -17,11 +17,23 @@ const initiateNewSession = async (req) => {
   // Generate a unique session ID using UUID
   const sessionId = uuidv4();
 
-  const clientIp = req.ip;
+  // Get the client IP address and user-agent from the request headers
+  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const userAgent = req.headers['user-agent'];
 
+  // Check for valid IP and user-agent
+  if (!clientIp || !userAgent) {
+    throw new Error("Invalid client IP or user-agent");
+  }
+
   // Get geolocation data
-  let geoInfo = await getGeolocation(clientIp);
+  let geoInfo;
+  try {
+    geoInfo = await getGeolocation(clientIp);
+  } catch (error) {
+    console.error(`Failed to fetch geolocation for IP: ${clientIp}`, error);
+    geoInfo = {};  // Default empty object if geolocation fetch fails
+  }
 
   // Determine device type
   let deviceType = "desktop";
