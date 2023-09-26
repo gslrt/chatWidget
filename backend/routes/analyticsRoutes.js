8a658ac207d5b73ae9352341b25e5e00cc1881d0
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-26 13:42:15 +0200

    Update analyticsRoutes.js

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -11,8 +11,8 @@ const pool = new Pool({
 
 // Function to update analytics database and session
 const updateAnalyticsDatabaseAndSession = async (req, eventType, eventData) => {
-  const clientIp = req.ip; // Get client IP address
-  const userAgent = req.headers['user-agent']; // Get User-Agent string
+  const clientIp = req.ip;
+  const userAgent = req.headers['user-agent'];
 
   // Get geolocation data
   let geoInfo = await getGeolocation(clientIp);
@@ -30,8 +30,8 @@ const updateAnalyticsDatabaseAndSession = async (req, eventType, eventData) => {
 
   // Update sessions table if it's a new session
   if (req.session.isNew) {
-    const sessionQuery = 'INSERT INTO website_analytics_sessions(session_id, user_ip, user_agent, start_timestamp, site, referrer_url, interaction_type, conversion_flag, city, country, state_prov, local_time, device_type) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)';
-    await pool.query(sessionQuery, [sessionId, clientIp, userAgent, new Date(), /* site, referrer_url, interaction_type, conversion_flag, */ geoInfo.city, geoInfo.country, geoInfo.state_prov, geoInfo.local_time, deviceType]);
+    const sessionQuery = 'INSERT INTO website_analytics_sessions(session_id, user_ip, user_agent, start_timestamp, city, country, state_prov, local_time, device_type) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)';
+    await pool.query(sessionQuery, [sessionId, clientIp, userAgent, new Date(), geoInfo.city, geoInfo.country, geoInfo.state_prov, geoInfo.local_time, deviceType]);
   }
 
   // Store event in database
@@ -39,29 +39,10 @@ const updateAnalyticsDatabaseAndSession = async (req, eventType, eventData) => {
   await pool.query(eventQuery, [sessionId, eventType, JSON.stringify(eventData)]);
 };
 
-
-router.post('/page_view', async (req, res) => {
-  const { url, referrer } = req.body;
-  await updateAnalyticsDatabaseAndSession(req, 'page_view', { url, referrer });
-  res.status(200).send('Page view recorded');
-});
-
-router.post('/heartbeat', async (req, res) => {
-  const { timestamp } = req.body;
-  await updateAnalyticsDatabaseAndSession(req, 'heartbeat', { timestamp });
-  res.status(200).send('Heartbeat recorded');
-});
-
-router.post('/interaction', async (req, res) => {
-  const { trigger, event_type } = req.body;
-  await updateAnalyticsDatabaseAndSession(req, 'interaction', { trigger, event_type });
-  res.status(200).send('Interaction recorded');
-});
-
-router.post('/element_scrolled', async (req, res) => {
-  const { trigger } = req.body;
-  await updateAnalyticsDatabaseAndSession(req, 'element_scrolled', { trigger });
-  res.status(200).send('Element scrolled into view recorded');
+router.post('/analytics', async (req, res) => {
+  const { eventType, additionalInfo } = req.body;
+  await updateAnalyticsDatabaseAndSession(req, eventType, additionalInfo);
+  res.status(200).send('Event recorded');
 });
 
 module.exports = router;
