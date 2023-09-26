02bbaf981b32407027eb3dacdfa2f5d18a6bf30e
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-26 21:51:23 +0200

    Update WebsiteAnalyticsServerSide.js

--- backend/WebsiteAnalyticsServerSide.js
+++ backend/WebsiteAnalyticsServerSide.js
@@ -16,29 +16,34 @@ const pool = new Pool({
 const initiateNewSession = async (req) => {
   // Generate a unique session ID using UUID
   const sessionId = uuidv4();
-  if (!sessionId) {
-    throw new Error('Failed to generate a valid UUID for the session.');
+
+  // Use the X-Forwarded-For header to get the real client IP
+  const clientIp = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() ||
+                   req.connection.remoteAddress || 
+                   req.socket.remoteAddress || 
+                   req.connection.socket.remoteAddress || 
+                   "Unknown";
+
+  if (clientIp === "Unknown" || clientIp.startsWith("192.168.")) {
+    console.error('IP address is not set or internal.');
+    return;
   }
 
-  // Get the client IP address and user-agent from the request headers
-  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const userAgent = req.headers['user-agent'];
 
-  // Check for valid IP and user-agent
-  if (!clientIp || !userAgent) {
-    throw new Error("Invalid client IP or user-agent");
+  // Get geolocation data
+  let geoInfo = await getGeolocation(clientIp);
+
+  if (!geoInfo) {
+    console.error("Failed to get geolocation: Geolocation API returned null");
+    return;
   }
 
-    // Get geolocation data
-  let geoInfo = await getGeolocation(clientIp);
-  if (!geoInfo) {
-    geoInfo = {
-      city: 'Unknown',
-      country: 'Unknown',
-      state_prov: 'Unknown',
-      local_time: new Date().toISOString()
-    };
-  }
+  const city = geoInfo.city || "Unknown";
+  const country = geoInfo.country_name || "Unknown";
+  const state_prov = geoInfo.state_prov || "Unknown";
+  const localTime = geoInfo.time_zone ? geoInfo.time_zone.current_time : "Unknown";
+
   // Determine device type
   let deviceType = "desktop";
   if (/mobile/i.test(userAgent)) {
@@ -49,9 +54,10 @@ const initiateNewSession = async (req) => {
 
   // SQL query to insert into the sessions table
   const sessionQuery = 'INSERT INTO website_analytics_sessions(session_id, user_ip, user_agent, start_timestamp, city, country, state_prov, local_time, device_type) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)';
-  await pool.query(sessionQuery, [sessionId, clientIp, userAgent, new Date(), geoInfo.city, geoInfo.country, geoInfo.state_prov, geoInfo.local_time, deviceType]);
+  await pool.query(sessionQuery, [sessionId, clientIp, userAgent, new Date(), city, country, state_prov, localTime, deviceType]);
 
   return sessionId;
 };
+
 
 module.exports = { initiateNewSession };
