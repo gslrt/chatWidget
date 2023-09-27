1d4d4a7fc16aa94c0cb85a74b784467e23852b73
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-27 10:58:41 +0200

    Update analyticsDatabaseUtils.js

--- backend/analyticsDatabaseUtils.js
+++ backend/analyticsDatabaseUtils.js
@@ -11,29 +11,6 @@ const pool = new Pool({
   }
 });
 
-// Function to initiate a new session and update the analytics database
-const initiateNewSession = async (req) => {
-  const sessionId = uuidv4();
-  const clientIp = req.ip;
-  const userAgent = req.headers['user-agent'];
-  
-  // Get geolocation data
-  let geoInfo = await getGeolocation(clientIp);
-
-  // Determine device type
-  let deviceType = "desktop";
-  if (/mobile/i.test(userAgent)) {
-    deviceType = "mobile";
-  } else if (/tablet|ipad|playbook|silk/i.test(userAgent)) {
-    deviceType = "tablet";
-  }
-
-  // SQL query to insert into the sessions table
-  const sessionQuery = 'INSERT INTO website_analytics_sessions(session_id, user_ip, user_agent, start_timestamp, city, country, state_prov, local_time, device_type) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)';
-  await pool.query(sessionQuery, [sessionId, clientIp, userAgent, new Date(), geoInfo.city, geoInfo.country, geoInfo.state_prov, geoInfo.local_time, deviceType]);
-
-  return sessionId;
-};
 
 // Function to update analytics database and session
 const updateAnalyticsDatabaseAndSession = async (req, eventType, eventData) => {
