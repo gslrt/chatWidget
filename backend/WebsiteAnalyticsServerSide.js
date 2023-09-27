e8536eb6901b89bd3a0013c56646608c206ec5b8
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-27 22:15:27 +0200

    Update WebsiteAnalyticsServerSide.js
    removed logging, lets see

--- backend/WebsiteAnalyticsServerSide.js
+++ backend/WebsiteAnalyticsServerSide.js
@@ -1,6 +1,3 @@
-
-
-
 const { v4: uuidv4 } = require('uuid');
 const { Pool } = require('pg');
 const { getGeolocation } = require('./geolocation');
@@ -15,13 +12,12 @@ const pool = new Pool({
 // Function to initiate a new session and update the analytics database
 const initiateNewSession = async (req) => {
   try {
-   // Capture the site and referrer from the request payload or headers
-  const site = req.body.site || req.headers.host || 'Unknown';
-  const referrer = req.body.referrer || req.headers.referer || 'Unknown';
+    // Capture the site and referrer from the request payload or headers
+    const site = req.body.site || req.headers.host || 'Unknown';
+    const referrerUrl = req.body.referrer || req.headers.referer || 'Unknown'; // Capture the referrer
 
     // Generate a unique session ID using UUID
     const sessionId = uuidv4();
-    console.log("Generated UUID:", sessionId);
 
     // Use the X-Forwarded-For header to get the real client IP
     const clientIp = (req.headers['x-forwarded-for'] || '').split(',')[0].trim() || 
@@ -30,20 +26,11 @@ const initiateNewSession = async (req) => {
                      req.connection.socket.remoteAddress || 
                      "Unknown";
 
-    console.log("Client IP:", clientIp);
-
-    if (clientIp === "Unknown" || clientIp.startsWith("192.168.") || clientIp.startsWith("::ffff:192.168.")) {
-      console.error('IP address is not set or internal.');
-      return;
-    }
-
     // Get user agent from headers
     const userAgent = req.headers['user-agent'];
-    console.log(`User Agent: ${userAgent}`);
 
     // Fetch geolocation data
     let geoInfo = await getGeolocation(clientIp);
-    console.log(`Geolocation Info: ${JSON.stringify(geoInfo)}`);
 
     // Check for null geolocation info
     if (!geoInfo) {
@@ -65,18 +52,9 @@ const initiateNewSession = async (req) => {
       deviceType = "tablet";
     }
 
-    // Debugging: Print all headers to console
-    console.log("Request headers: ", JSON.stringify(req.headers));
-
-    // Capture referrer
-    const referrerUrl = req.body.additionalInfo?.referrer || req.headers.referer || 'Unknown';
-
     // SQL query to insert the session into the database, now including referrerUrl
     const sessionQuery = 'INSERT INTO website_analytics_sessions(session_id, user_ip, user_agent, start_timestamp, city, country, state_prov, local_time, device_type, country_flag, site, referrer_url) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)';
     const queryParams = [sessionId, clientIp, userAgent, new Date(), city, country, state_prov, localTime, deviceType, countryFlag, site, referrerUrl];
-
-    console.log(`Executing query: ${sessionQuery}`);
-    console.log(`With values: ${JSON.stringify(queryParams)}`);
 
     // Execute the query
     await pool.query(sessionQuery, queryParams);
