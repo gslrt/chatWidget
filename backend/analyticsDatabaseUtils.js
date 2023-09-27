cbecfd8cbc08933ea802d5cd045af5bb829dd917
gslrt <140710359+gslrt@users.noreply.github.com>
2023-09-27 11:19:02 +0200

    Update analyticsDatabaseUtils.js

--- backend/analyticsDatabaseUtils.js
+++ backend/analyticsDatabaseUtils.js
@@ -2,7 +2,6 @@
 
 const { v4: uuidv4 } = require('uuid');
 const { Pool } = require('pg');
-const { getGeolocation } = require('../geolocation');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
