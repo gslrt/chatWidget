6e31299dece3028529e5d519300aee45fd59324d
gslrt <140710359+gslrt@users.noreply.github.com>
2023-10-03 12:15:33 +0200

    Update tether.js

--- frontend/src/tether.js
+++ frontend/src/tether.js
@@ -1,33 +1,24 @@
+// frontend/src/tether.js
+
 // Function to load additional scripts asynchronously
-function loadScript(src, callback) {
+function loadScript(src) {
   var script = document.createElement('script');
   script.async = true;
   script.src = src;
-
-  // Run the callback when the script is loaded
-  script.onload = function() {
-    if (callback) callback();
-  };
-
   document.body.appendChild(script);
 }
 
 // Main function to handle tracking and dynamic script loading
 (function() {
-  // Load the analytics script first
-  loadScript('https://chatwidget-production.up.railway.app/frontend/dist/analytics.bundle.js', function() {
-    // Analytics script has loaded, check for session ID
-    const checkForSessionId = setInterval(function() {
-      const sessionId = sessionStorage.getItem("sessionId");
-      if (sessionId) {
-        clearInterval(checkForSessionId);
-        
-        // Session ID is available, now load the native chat bundle
-        const chatElement = document.querySelector('[embed-element="native-chat"]');
-        if (chatElement) {
-          loadScript('https://chatwidget-production.up.railway.app/frontend/dist/native.bundle.js');
-        }
-      }
-    }, 100);
-  });
+  // Check for data attributes for chat embedding and widgets
+  const chatElement = document.querySelector('[embed-element="native-chat"]');
+  if (chatElement) {
+    // Load the native chat bundle asynchronously
+    loadScript('https://chatwidget-production.up.railway.app/frontend/dist/native.bundle.js');
+  }
+
+  // Load the analytics script asynchronously
+  loadScript('https://chatwidget-production.up.railway.app/frontend/dist/analytics.bundle.js');
+  
+
 })();
