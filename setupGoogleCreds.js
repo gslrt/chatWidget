838a7f0462c7d46476d1ff5f23498a8cfaa7ebe3
gslrt <140710359+gslrt@users.noreply.github.com>
2023-10-08 11:44:39 +0200

    Update setupGoogleCreds.js

--- setupGoogleCreds.js
+++ setupGoogleCreds.js
@@ -3,9 +3,21 @@ const path = require('path');
 
 function setupGoogleCredentials() {
   const creds = process.env.GOOGLE_CREDENTIALS_JSON;
+  console.log("GOOGLE_CREDENTIALS_JSON exists:", !!creds);
+  
   const credsObj = JSON.parse(creds);
   const credsPath = path.join(process.env.RAILWAY_VOLUME_MOUNT_PATH, 'google-creds.json');
+  console.log("Generated credsPath:", credsPath);
+  
   fs.writeFileSync(credsPath, JSON.stringify(credsObj, null, 2));
+  
+  if (fs.existsSync(credsPath)) {
+      console.log("Credentials file exists.");
+      console.log("File content:", fs.readFileSync(credsPath, 'utf8'));
+  } else {
+      console.log("Credentials file does not exist.");
+  }
+  
   process.env.GOOGLE_APPLICATION_CREDENTIALS = credsPath;
 }
 
