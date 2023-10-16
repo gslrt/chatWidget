bb7e15f50cdc1914fa057f0dd7068916594e989d
gslrt <140710359+gslrt@users.noreply.github.com>
2023-10-16 13:16:35 +0200

    Update sharedBundle.js

--- frontend/src/shared/sharedBundle.js
+++ frontend/src/shared/sharedBundle.js
@@ -182,23 +182,18 @@ export function sharedFunction() {
 
 
 function chatRecording() {
-    console.log("Debug: Inside chatRecording function"); // Debug line
     let mediaRecorder = null;
     let audioChunks = [];
     let recording = false;
     let stream;
 
-    // Corresponding HTML elements
     const triggerDiv = document.querySelector('div[trigger-action="toggle-audio-record-chat-input"]');
-    console.log("Debug: Preparing to add event listener to:", triggerDiv); // Debug line
-
     const input = document.querySelector('div[element="chat-user-input"]');
     const canvas = document.querySelector('canvas[element="audio-recording-visualizer"]');
     const canvasContainer = document.querySelector('div[element="audio-recording-visualizer-code"]');
     const canvasCtx = canvas.getContext('2d');
 
-    // Existing configurations
-    canvasContainer.style.display = "none"; 
+    canvasContainer.style.display = "none";
     let audioCtx;
     let source;
     let analyser;
@@ -240,48 +235,42 @@ function chatRecording() {
 
             canvasCtx.lineTo(canvas.width, canvas.height / 2);
             canvasCtx.stroke();
-        };
+        }
 
         draw();
-    }
-    const toggleRecording = async () => {
-        console.log("Debug: Inside toggleRecording function"); // Debug line
+    };
 
+    const toggleRecording = async () => {
         if (recording) {
-            console.log("Debug: Stopping recording"); // Debug line
             if (mediaRecorder && mediaRecorder.state === 'recording') {
                 mediaRecorder.stop();
                 mediaRecorder.stream.getTracks().forEach(track => track.stop());
                 triggerDiv.style.backgroundColor = '';
-                canvasContainer.style.display = "none"; 
+                canvasContainer.style.display = "none";
             }
         } else {
-            console.log("Debug: Starting recording"); // Debug line
             stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
             mediaRecorder = new MediaRecorder(stream);
             initAudioContext();
 
-            input.textContent = ''; 
+            input.textContent = '';
 
             mediaRecorder.ondataavailable = event => audioChunks.push(event.data);
 
             mediaRecorder.onstop = () => {
-    const formData = new FormData();
-    formData.append('file', new Blob(audioChunks, { type: 'audio/webm' }), 'audio.webm');
+                const formData = new FormData();
+                formData.append('file', new Blob(audioChunks, { type: 'audio/webm' }), 'audio.webm');
+                const token = localStorage.getItem('token');
+                const transcribeApiUrl = `${process.env.TRANSCRIBE_SERVER_URL}/transcribe`;
 
-    const token = localStorage.getItem('token');
-
-    // Use process.env.TRANSCRIBE_SERVER_URL directly
-    const transcribeApiUrl = `${process.env.TRANSCRIBE_SERVER_URL}/transcribe`;
-
-    fetch(transcribeApiUrl, {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer ' + token,
-        },
-        body: formData,
-        credentials: 'include'
-    })
+                fetch(transcribeApiUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': 'Bearer ' + token,
+                    },
+                    body: formData,
+                    credentials: 'include'
+                })
                 .then(response => response.json())
                 .then(data => {
                     if (data.text) {
@@ -295,31 +284,24 @@ function chatRecording() {
                 });
 
                 audioChunks = [];
-    };
+            };
 
             mediaRecorder.start();
             triggerDiv.style.backgroundColor = 'red';
-            canvasContainer.style.display = "block"; 
+            canvasContainer.style.display = "block";
         }
 
         recording = !recording;
     };
 
-    if (triggerDiv) {  // Check if triggerDiv exists
-        console.log("Debug: Adding event listener to triggerDiv"); // Debug line
+    if (triggerDiv) {
         triggerDiv.addEventListener('click', toggleRecording);
-    } else {
-        console.log("Debug: triggerDiv not found"); // Debug line
     }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    chatRecording(); // Initialize your new chat recording functionality
+    chatRecording();
 });
-
-
-
-
 
 
 
