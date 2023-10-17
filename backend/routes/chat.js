78e80520c513a732a59523efeec369ba357bd210
gslrt <140710359+gslrt@users.noreply.github.com>
2023-10-17 10:10:01 +0200

    Update chat.js

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -8,12 +8,11 @@ const IPGeolocationAPI = require('ip-geolocation-api-javascript-sdk');
 const { getGeolocation } = require('../geolocation');
 const ipgeolocationApi = new IPGeolocationAPI(process.env.GEOLOCATOR_API_KEY, false);
 
-
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Function to update database and session
@@ -59,82 +58,85 @@ const updateDatabaseAndSession = async (socket, currentTimestamp, userInput, aiR
 
 
 
+
 router.handleSocketConnection = (socket, uid) => {
-    console.log(`[Chat Route] User ${uid} connected: ${socket.id}`);
+  console.log(`[Chat Route] User ${uid} connected: ${socket.id}`);
 
-    // Capture the session ID from the client
-    socket.on('setSessionId', (sessionId) => {
-        socket.request.session.sessionID = sessionId;
-    });
-    
-    socket.on('chatMessage', async (data) => {
-        try {
+  // Capture the session ID from the client
+  socket.on('setSessionId', (sessionId) => {
+    socket.request.session.sessionID = sessionId;
+  });
 
+  socket.on('chatMessage', async (data) => {
+    try {
+      // Enable TTS by default
+      data.ttsEnabled = true;
 
-            // Enable TTS by default
-        data.ttsEnabled = true;
-            
-            console.log("[Chat Route] Received chat message:", data);
-            const userInput = data.question;
-            const currentTimestamp = new Date();
+      console.log("[Chat Route] Received chat message:", data);
+      const userInput = data.question;
+      const chatMode = data.mode; // Added chatMode
 
-            // Hardcoded role
-            const role = 'public';
+      // Determine maxTokens based on chatMode
+      let maxTokens = 300; // Default
+      if (chatMode === 'B') {
+        maxTokens = 100; // Special case for Mode B
+      }
 
-            // Determine hiveAccess based on hardcoded role
-            let hiveAccess;
-            switch (role) {
-                case 'public':
-                    hiveAccess = process.env.HIVE_ACCESS_PUBLIC;
-                    break;
-                default:
-                    socket.emit('error', { error: 'Invalid role' });
-                    return;
-            }
+      const currentTimestamp = new Date();
+      const role = 'public';
+      
+      let hiveAccess;
+      switch (role) {
+        case 'public':
+          hiveAccess = process.env.HIVE_ACCESS_PUBLIC;
+          break;
+        default:
+          socket.emit('error', { error: 'Invalid role' });
+          return;
+      }
 
-            const url = process.env.CHAT_URL + hiveAccess;
-
-            let systemMessage = 'you are a pirate';
-
-            const dataToSend = {
-                question: userInput,
-                overrideConfig: {
-                    systemMessage,
-                    openAIApiKey: process.env.OPENAI_API_KEY,
-                    pineconeEnv: process.env.PINECONE_ENVIRONMENT,
-                    pineconeApiKey: process.env.PINECONE_API_KEY,
-                    pineconeNamespace: process.env.PINECONE_NAMESPACE,
-                    pineconeIndex: process.env.PINECONE_INDEX_NAME
-                }
-            };
-
-            const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                    "Authorization": `Bearer ${process.env.FLOWISE_API_KEY}`,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(dataToSend)
-            });
-
-            const responseBody = await response.json();
-            let aiResponse = responseBody;
-
-            if (data.ttsEnabled) {
-                let audioUrl = await generateAudio(aiResponse);
-                socket.emit('botResponse', { 'text': aiResponse, 'audioUrl': audioUrl });
-            } else {
-                socket.emit('botResponse', { 'text': aiResponse });
-            }
-
-            // Call the updateDatabaseAndSession function
-            await updateDatabaseAndSession(socket, currentTimestamp, userInput, aiResponse);
-
-        } catch (error) {
-            console.error('Error:', error);
-            socket.emit('error', { error: 'An error occurred' });
+      const url = process.env.CHAT_URL + hiveAccess;
+      let systemMessage = 'you are a pirate';
+      
+      // Include maxTokens in the dataToSend
+      const dataToSend = {
+        question: userInput,
+        overrideConfig: {
+          maxTokens, // Added maxTokens
+          systemMessage,
+          openAIApiKey: process.env.OPENAI_API_KEY,
+          pineconeEnv: process.env.PINECONE_ENVIRONMENT,
+          pineconeApiKey: process.env.PINECONE_API_KEY,
+          pineconeNamespace: process.env.PINECONE_NAMESPACE,
+          pineconeIndex: process.env.PINECONE_INDEX_NAME
         }
-    });
+      };
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${process.env.FLOWISE_API_KEY}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataToSend)
+      });
+
+      const responseBody = await response.json();
+      let aiResponse = responseBody;
+
+      if (data.ttsEnabled) {
+        let audioUrl = await generateAudio(aiResponse);
+        socket.emit('botResponse', { 'text': aiResponse, 'audioUrl': audioUrl });
+      } else {
+        socket.emit('botResponse', { 'text': aiResponse });
+      }
+
+      await updateDatabaseAndSession(socket, currentTimestamp, userInput, aiResponse);
+    } catch (error) {
+      console.error('Error:', error);
+      socket.emit('error', { error: 'An error occurred' });
+    }
+  });
 };
 
 module.exports = router;
