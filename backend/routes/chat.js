d06761afb6b03760610c3165e936263d15245cb3
gslrt <140710359+gslrt@users.noreply.github.com>
2023-10-26 15:01:55 +0200

    Update chat.js

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -64,6 +64,7 @@ router.handleSocketConnection = (socket, uid) => {
 
   // New code to listen for token streaming from Flowise
   flowiseSocket.on('token', (token) => {
+      console.log("Received token from Flowise:", token);  // Debugging line
     if (socket.chatMode === 'C') {
       socket.emit('token', token);
     }
