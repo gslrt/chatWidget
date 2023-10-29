8abfb540f1aba6562807fd76cd7adeae356c991e
gslrt <140710359+gslrt@users.noreply.github.com>
2023-10-29 16:28:34 +0100

    Update sharedBundle.js

--- frontend/src/shared/sharedBundle.js
+++ frontend/src/shared/sharedBundle.js
@@ -356,29 +356,37 @@ botMessageElement.classList.add('message-visible');
 
    thinkingStateElement.style.display = 'none';
   
-    // Special handling for Conversation Mode
-  if (currentMode === 'B') {
-    const conversationModeTextBlock = document.querySelector('[element="bot-response-conversation-mode"]');
-    if (conversationModeTextBlock) {
-      // Fade out current content
-      conversationModeTextBlock.classList.remove('visible');
-      conversationModeTextBlock.classList.add('fade');
-
-      // Wait for the fade-out to complete, then update and fade in
+  // Special handling for Conversation Mode
+if (currentMode === 'B') {
+  const conversationModeTextBlock = document.querySelector('[element="bot-response-conversation-mode"]');
+  if (conversationModeTextBlock) {
+    
+    // Fade out current content
+    conversationModeTextBlock.classList.remove('visible');
+    conversationModeTextBlock.classList.add('fade');
+    
+    // Remove the 'fade' class after the fade-out animation (assumed to be 500ms)
+    setTimeout(() => {
+      conversationModeTextBlock.classList.remove('fade');
+    }, 500);
+    
+    // Wait for the fade-out to complete, then update and fade in
+    setTimeout(() => {
+      conversationModeTextBlock.innerHTML = data.text;
+      conversationModeTextBlock.classList.remove('fade');
+      conversationModeTextBlock.classList.add('visible', 'fade-in');
+      
+      // Remove the 'fade-in' class after the animation (1s) to reset for the next message
       setTimeout(() => {
-        conversationModeTextBlock.innerHTML = data.text;
-        conversationModeTextBlock.classList.remove('fade');
-        conversationModeTextBlock.classList.add('visible');
-
-        // Add the fade-in class for fade-in effect
-        conversationModeTextBlock.classList.add('fade-in');
-
-      }, 500); // Should match the duration in the CSS transition
-    } else {
-      console.error("Couldn't find the text block for Conversation Mode.");
-    }
+        conversationModeTextBlock.classList.remove('fade-in');
+      }, 1000);
+      
+    }, 500);  // Should match the duration in the CSS transition
+  } else {
+    console.error("Couldn't find the text block for Conversation Mode.");
   }
-});
+}
+
 
 
 
